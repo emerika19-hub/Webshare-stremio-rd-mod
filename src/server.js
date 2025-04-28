0a4796a75f3dd7c096a0211cef3301def4d068a6
Alexuspo <86742943+Alexuspo@users.noreply.github.com>
2025-04-28 08:14:41 +0200

    heroku server update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
 const addonInterface = require("./addon")
 
-// Použij PORT z proměnných prostředí, pokud existuje (pro Vercel a další platformy)
+// Použij PORT z proměnných prostředí, pokud existuje (pro Heroku, Vercel a další platformy)
 // nebo použij výchozí port 7000
 const port = process.env.PORT || 7000
 
@@ -17,11 +17,21 @@ if (isVercel) {
     addonInterface.handle(req, res)
   }
 } else {
-  // Lokální vývojový server
-  serveHTTP(addonInterface, { port: port, host: '0.0.0.0' })
+  // Lokální vývojový server nebo Heroku
+  console.log(`Spouštím HTTP server na portu ${port}`)
+  serveHTTP(addonInterface, { 
+    port: port, 
+    host: '0.0.0.0',  // Důležité pro Heroku - poslouchá na všech rozhraních
+    stremioReady: true // Pro kompatibilitu s některými hostingovými službami
+  })
   console.log(`Addon běží na http://127.0.0.1:${port}/manifest.json`)
+
+  // Detekce prostředí Heroku (pro logování)
+  if (process.env.DYNO) {
+    console.log('Běžím v prostředí Heroku')
+  }
 }
 
 // Pro publikování addonu do centrálního adresáře Stremio, odkomentujte následující řádek
 // a nahraďte URL adresu skutečnou adresou vašeho nasazeného addonu
-// publishToCentral("https://vas-addon-webshare.vercel.app/manifest.json")
+// publishToCentral("https://vas-addon-webshare.herokuapp.com/manifest.json")
